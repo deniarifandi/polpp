4d6bd1b702f872a567e2f3b2ac70d5cef3148e2b
deniarifandi <42629835+deniarifandi@users.noreply.github.com>
2022-05-02 21:33:00 +0700

    logged in names

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -218,14 +218,14 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+            <img src="assets/img/user_default.png" alt="Profile" class="rounded-circle">
+            <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-              <h6>Kevin Anderson</h6>
-              <span>Web Designer</span>
+              <h6>{{ Auth::user()->name }}</h6>
+              {{-- <span>Web Designer</span> --}}
             </li>
             <li>
               <hr class="dropdown-divider">
@@ -267,7 +267,7 @@
                 <a class="dropdown-item d-flex align-items-center" href="{{ route('logout') }}"  onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                   <i class="bi bi-box-arrow-right"></i>
                     <span>Sign Out</span>
-                </a>
+                </a>  
 
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                     @csrf
