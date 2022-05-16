0b303e799e1154336aa14064f82a1d3ad7388163
deniarifandi <arifandi.malang@gmail.com>
2022-05-16 07:17:24 +0700

    Update detail_pelanggaran.blade.php

--- resources/views/detail_pelanggaran.blade.php
+++ resources/views/detail_pelanggaran.blade.php
@@ -5,7 +5,8 @@
 @section('title','tambah pelanggaran')
 
 @section('content')
-
+    
+    <script type="text/javascript" src="https://unpkg.com/webcam-easy/dist/webcam-easy.min.js"></script>
 
     <div class="pagetitle">
       <h1>Detail Page</h1>
@@ -245,12 +246,8 @@
                     {{-- <button type="submit" class="btn btn-success">Simpan & Lanjutkan</button> --}}
                   </form>
 
-                      <button id="start-camera">Start Camera</button>
-<video id="video" width="320" height="240" autoplay></video>
-<button id="click-photo">Click Photo</button>
-<canvas id="canvas" width="320" height="240"></canvas>
-                    
-              
+                  <input type="file" accept="image/*" capture="camera" />
+
                 </tbody>
               </table>
             
@@ -266,23 +263,10 @@
 
     <script type="text/javascript">
         
-        let camera_button = document.querySelector("#start-camera");
-        let video = document.querySelector("#video");
-        let click_button = document.querySelector("#click-photo");
-        let canvas = document.querySelector("#canvas");
 
-        camera_button.addEventListener('click', async function() {
-            let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-          video.srcObject = stream;
-        });
+    
 
-        click_button.addEventListener('click', function() {
-            canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-            let image_data_url = canvas.toDataURL('image/jpeg');
-
-            // data url of the image
-            console.log(image_data_url);
-        });
+    
 
     </script>
 
