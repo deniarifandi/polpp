841b77da568726ca10b83f020fc2297c1c49a6cb
deniarifandi <arifandi.malang@gmail.com>
2022-07-22 09:52:43 +0700

    Update front.blade.php

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -441,9 +441,19 @@
         })();
 
 
+        $("form").each(function() {
+            $(this).find(':input[type="submit"]').prop('disabled', true);
+        });
+        function correctCaptcha() {
+            $("form").each(function() {
+                $(this).find(':input[type="submit"]').prop('disabled', false);
+            });
+        }
+
 
     </script>
     
+
 
 
 </body>
