7b0908507e1436f48b8174bcf2b0fb62262ff331
deniarifandi <arifandi.malang@gmail.com>
2022-07-22 08:42:55 +0700

    Update LaporanController.php

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -45,7 +45,7 @@ class LaporanController extends Controller
 
       Storage::disk('public')->put("foto_laporan/".$laporan->id, $request->fotolaporan);
 
-		// return redirect('/');
+		  return redirect('/');
     }
    
 
