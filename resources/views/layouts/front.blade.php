7c81e52c9b1b5385a69010c7d3119919c56b6e22
deniarifandi <arifandi.malang@gmail.com>
2022-10-09 15:09:35 +0700

    sosmed

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -385,11 +385,11 @@
         </div>
 
         <div class="social-links">
-          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+          <a href="https://twitter.com/satpolppmalang2" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="tel:0341353939" class="phone"><i class="bi bi-phone"></i></a>
+          <a href="https://www.instagram.com/satpolpp.malangkota/" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <!--  <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a> -->
+         <!-- <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a> -->
         </div>
 
       </div>
